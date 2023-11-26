ef5d5a8be607a372d5723a626224d48a11db7104
sumiremama <yumi.kikuchi.0621@gmail.com>
2023-11-26 21:20:01 +0900

    修正1

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '用事' },
-    { id: 3, name: '買い物' },
+    { id: 2, name: 'やることリスト' },
+    { id: 3, name: '買い物リスト' },
     { id: 4, name: 'その他' }
   ]
 
